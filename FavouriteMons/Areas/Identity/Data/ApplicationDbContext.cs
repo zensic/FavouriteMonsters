c7cd8ba94f06d432b3ef89875c48160aca1da1e6
Anthony Lee <anthonyleehj@gmail.com>
2022-07-14 13:19:44 +0800

    Add project files.

---
+++ FavouriteMons/Areas/Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,50 @@
+﻿using FavouriteMons.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FavouriteMons.Areas.Identity.Data;
+
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<IdentityUser>(entity =>
+        {
+            entity.ToTable(name: "Users");
+        });
+        builder.Entity<IdentityRole>(entity =>
+        {
+            entity.ToTable(name: "Roles");
+        });
+        builder.Entity<IdentityUserRole<string>>(entity =>
+        {
+            entity.ToTable("UserRoles");
+        });
+        builder.Entity<IdentityUserClaim<string>>(entity =>
+        {
+            entity.ToTable("UserClaims");
+        });
+        builder.Entity<IdentityUserLogin<string>>(entity =>
+        {
+            entity.ToTable("UserLogins");
+        });
+        builder.Entity<IdentityRoleClaim<string>>(entity =>
+        {
+            entity.ToTable("RoleClaims");
+        });
+        builder.Entity<IdentityUserToken<string>>(entity =>
+        {
+            entity.ToTable("UserTokens");
+        });
+    }
+
+    public DbSet<Teams> Teams { get; set; }
+}
---
+++ FavouriteMons/Models/Teams.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FavouriteMons.Models
+{
+    public class Teams
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Users")]
+        public int UserId { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public string Pokemon { get; set; }
+    }
+}
