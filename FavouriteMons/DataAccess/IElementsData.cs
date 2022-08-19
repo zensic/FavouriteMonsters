e5acc04e1a0ffd83ad2c263e5e99fd2d1403006c
Anthony Lee <anthonyleehj@gmail.com>
2022-08-19 14:14:40 +0800

    Added temporary token for api authorization

--- FavouriteMons/DataAccess/IElementsData.cs
+++ FavouriteMons/DataAccess/IElementsData.cs
@@ -4,9 +4,10 @@ using Refit;
 
 namespace FavouriteMons.DataAccess
 {
-    public interface IElementsData
-    {
-        [Get("/Elements")]
-        Task<List<Elements>> GetElements();
-    }
+  [Headers("Authorization: 52cc004d-b5c8-448f-be6b-6b9bd532fe20")]
+  public interface IElementsData
+  {
+    [Get("/Elements")]
+    Task<List<Elements>> GetElements();
+  }
 }
--- FavouriteMons/DataAccess/IMonstersData.cs
+++ FavouriteMons/DataAccess/IMonstersData.cs
@@ -3,21 +3,22 @@ using Refit;
 
 namespace FavouriteMons.DataAccess
 {
-    public interface IMonstersData
-    {
-        [Get("/Monsters")]
-        Task<IEnumerable<object>> GetMonsters();
+  [Headers("Authorization: 52cc004d-b5c8-448f-be6b-6b9bd532fe20")]
+  public interface IMonstersData
+  {
+    [Get("/Monsters")]
+    Task<IEnumerable<object>> GetMonsters();
 
-        [Post("/Monsters")]
-        Task CreateMonster([Body] Monsters monster);
+    [Post("/Monsters")]
+    Task CreateMonster([Body] Monsters monster);
 
-        [Get("/Monsters/{id}")]
-        Task<Monsters> GetMonsters(Guid id);
+    [Get("/Monsters/{id}")]
+    Task<Monsters> GetMonsters(Guid id);
 
-        [Put("/Monsters/{id}")]
-        Task UpdateMonster(Guid id, [Body] Monsters monster);
+    [Put("/Monsters/{id}")]
+    Task UpdateMonster(Guid id, [Body] Monsters monster);
 
-        [Delete("/Monsters/{id}")]
-        Task DeleteMonster(Guid id);
-    }
+    [Delete("/Monsters/{id}")]
+    Task DeleteMonster(Guid id);
+  }
 }
