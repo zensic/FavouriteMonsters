c6d786cc73333c4cc4b12dc038cc9806d7b8ecc5
Anthony Lee <anthonyleehj@gmail.com>
2022-09-02 16:48:41 +0800

    Updated login & register css

--- FavouriteMons/Controllers/HomeController.cs
+++ FavouriteMons/Controllers/HomeController.cs
@@ -1,32 +1,40 @@
-﻿using FavouriteMons.Models;
+﻿using FavouriteMons.Areas.Identity.Data;
+using FavouriteMons.DataAccess;
+using FavouriteMons.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace FavouriteMons.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
+    private readonly IMonstersData _monstersData;
+    private readonly IElementsData _elementsData;
+
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, IMonstersData monstersData, IElementsData elementsData)
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      _logger = logger;
+      _context = context;
+      _monstersData = monstersData;
+      _elementsData = elementsData;
     }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
 }
