d4aace830754eacdcd6418ed3a508855544dc2e5
Anthony Lee <anthonyleehj@gmail.com>
2022-09-05 15:03:30 +0800

    Updated monster creation

--- FavouriteMons/Models/Monsters.cs
+++ FavouriteMons/Models/Monsters.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace FavouriteMons.Models
 {
@@ -9,8 +10,16 @@ namespace FavouriteMons.Models
     [Required]
     public string Name { get; set; }
     [Required]
+    [DisplayName("Element")]
     public Guid ElementId { get; set; }
     public string ImageUrl { get; set; }
-    public int Votes { get; set; } = 0;
+    [Range(1, 255, ErrorMessage = "Value must be between 1 and 255")]
+    public int HP { get; set; }
+    [Range(1, 255, ErrorMessage = "Value must be between 1 and 255")]
+    public int Attack { get; set; }
+    [Range(1, 255, ErrorMessage = "Value must be between 1 and 255")]
+    public int Defence { get; set; }
+    [Range(1, 255, ErrorMessage = "Value must be between 1 and 255")]
+    public int Speed { get; set; }
   }
 }
